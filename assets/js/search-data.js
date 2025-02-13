7a54f39b7ddadf08c37701cf721439ccf3db7d1f
rinehartj <jasondoesminecraft@hotmail.com>
2025-02-13 02:52:51 +0000

    Deploying to gh-pages from @ rinehartj/rinehartj.github.io@26b59442bde930f6b967f82bb0db4ad120bcfc9e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -41,18 +41,7 @@ ninja.data = [{
           window.location.href = "/blog/2025/webupdates/";
         
       },
-    },{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "projects-custom-built-computer",
+    },{id: "projects-custom-built-computer",
           title: 'Custom-Built Computer',
           description: "Allowed smooth handling of resource-intense applications.",
           section: "Projects",handler: () => {
